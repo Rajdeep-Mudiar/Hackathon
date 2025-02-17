ebcc783bc612455b4645ab19e630497bb7445ba5
Rajdeep Mudiar <rajdeepmudiar01@gmail.com>
2025-02-17 09:19:29 +0530

    Add files via upload

---
+++ google_soln/finance_bot/backend.py
@@ -0,0 +1,32 @@
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
+load_dotenv()
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+# Create the model
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 40,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(
+  model_name="gemini-2.0-flash-exp",
+  generation_config=generation_config,
+)
+
+def GenerateResponse(input_text):
+    response = model.generate_content([
+    "you are a financial assistant  chatbot.so reply accordingly",
+    f"input:{input_text}",
+    "output: ",
+    ])
+    
+    return(response.text)
+# while True:
+#     string=str(input("Enter your promt:"))
+#     print("Bot: "GenerateResponse(string))
